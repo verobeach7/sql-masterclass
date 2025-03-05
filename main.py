be1316549f36041b39a86c2adf1800b2113f8445
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2025-03-05 20:43:22 +0900

    SQL-Masterclass: Redis Caching(with other DB)

--- main.py
+++ main.py
@@ -1,27 +1,41 @@
+import redis
 import sqlite3
 
-# In Memory Database
-# :memory: 를 이용하여 connection을 하면 메모리에 데이터베이스를 불러와서 Redis처럼 사용할 수 있게 됨
-conn = sqlite3.connect("movies.db")
+# 타입 변환에 사용
+import json
 
+r = redis.Redis(
+    host="localhost",
+    port=6379,
+    decode_responses=True,
+)
+
+conn = sqlite3.connect("movies.db")
 cur = conn.cursor()
 
-res = cur.execute("SELECT movie_id, title FROM movies;")
 
-# # Cursor는 fetch를 하면 어디까지 fetch했는지를 기억해 놓음. 위치를 기억하기 때문에 커서라고 함.
-# first_thousand = res.fetchmany(1000)  # 1000번째 row까지 fetch했음을 기억
-# next_thousand = res.fetchmany(10000)  # 11000번째 row까지 fetch했음을 기억
-# last_movies = res.fetchall()  # 11001번째 row부터 끝까지 가져옴
+# 쿼리하는데 비용이 많이 들어가는 상황이라고 가정
+def make_expensive_query():
+    redis_key = "director:movies"
+    cached_results = r.get(redis_key)
+    if cached_results:
+        print("Cache hit")
+        # Redis에는 String으로 데이터가 들어가 있으므로 Python Object로 변환해줘야 함
+        # json.loads(): string을 불러와 python object로 변환
+        return json.loads(cached_results)
+    else:
+        print("Cache miss")  # 캐시 없음
+        res = cur.execute("SELECT count(*), director FROM movies GROUP BY director;")
+        all_rows = res.fetchall()  # all_rows는 Python Object: Tuple 형태로 받게 됨
+        # Redis에 저장하기 위해서는 String 타입으로 바꿔줘야 함. String 타입으로만 저장 가능!
+        # json.dumps(): string으로 dump함
+        r.set(redis_key, json.dumps(all_rows), ex=20)  # ex: 20초 간 데이터 유지
+        return all_rows
 
-# print(
-#     res.fetchone(),
-#     res.fetchone(),
-#     res.fetchone(),
-#     res.fetchone(),
-# )
 
-for movie in res:
-    print(movie)
+v = make_expensive_query()
 
 conn.commit()
 conn.close()
+
+r.close()
