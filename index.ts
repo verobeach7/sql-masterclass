464ed9c5728a45a94b0c71774f93b23909cdef71
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2025-03-09 17:42:12 +0900

    SQL-Masterclass: Redis(Usecase with TypeScript)

--- index.ts
+++ index.ts
@@ -1,29 +1,24 @@
-import { Database } from "bun:sqlite";
-import { drizzle } from "drizzle-orm/bun-sqlite";
-import { comments, users } from "./schema";
-import { eq } from "drizzle-orm";
+import { createClient } from "redis";
 
-const sqlite = new Database("users.db");
+// 아무설정 없이 비워두면 default 값으로 localhost:6379로 연결
+const client = createClient();
 
-// logger: true 옵션을 주면 백그라운드에서 무슨 일이 벌어지고 있는지 로그를 보여줌
-const db = drizzle(sqlite, { logger: true });
+// bun을 사용하지 않고 npm이나, node.js를 사용하는 경우 await을 최상위에서 사용하지 못할 수도 있음
+// 이런 경우 await을 async 함수 안에 넣어서 처리해주면 됨
+await client.connect();
 
-// TypeScript의 자동완성 기능을 이용해 편리하게 쿼리를 완성할 수 있음
-// returning은 쿼리 결과를 받아볼 수 있게 해줌
-// const result = await db.insert(users).values({ username: "nico" }).returning();
+await client.set("hello", "world");
 
-// const result = await db
-//   .insert(comments)
-//   .values({ payload: "hello drizzle", userId: 1 })
-//   .returning();
+const r1 = await client.get("hello");
+console.log(r1);
 
-// SQL Injection 문제로부터 자유로움. 해킹 방지!
-// const result = await db.select().from(comments).where(eq(comments.userId, 1));
+await client.hSet("users:1", {
+  username: "nico",
+  password: "123",
+});
 
-// sqlite 경우에는 leftjoin과 rightjoin밖에 없지만 다른 데이터베이스에서 지원하는 join도 자동완성에 보여주기는 함
-const result = await db
-  .select({ user: users.username, comment: comments.payload })
-  .from(comments)
-  .leftJoin(users, eq(comments.userId, users.userId));
+const r2 = await client.hGetAll("users:1");
 
-console.log(result);
+console.log(r2);
+
+await client.disconnect();
